478d9de3a96b0931f6494ad5f090b3765ac8bbbb
Gabriel Stafoca <gabriel@waba.com.br>
2024-07-08 14:47:50 -0300

    feat: Docker was added

--- database/factories/NewsFactory.php
+++ database/factories/NewsFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\News;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 /**
